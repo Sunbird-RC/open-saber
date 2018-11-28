8dca682079a5aedb707e9549e6d5453af6d00e0f
indrajra-ilimi <rajeshr@ilimi.in>
2018-11-28 18:23:38 +0530

    Issue #OS-116 Get rid of validation errors

--- java/validators/json/jsonschema/src/main/java/io/opensaber/validators/json/jsonschema/JsonValidationServiceImpl.java
+++ java/validators/json/jsonschema/src/main/java/io/opensaber/validators/json/jsonschema/JsonValidationServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import io.opensaber.pojos.APIMessage;
 import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
 import org.everit.json.schema.loader.SchemaLoader;
 
 
@@ -15,6 +16,8 @@ import io.opensaber.registry.middleware.MiddlewareHaltException;
 import io.opensaber.validators.IValidate;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.core.io.ClassPathResource;
@@ -25,6 +28,7 @@ import org.springframework.web.context.support.ServletContextResource;
 import javax.servlet.ServletContext;
 
 public class JsonValidationServiceImpl implements IValidate {
+	private static Logger logger = LoggerFactory.getLogger(JsonValidationServiceImpl.class);
 
 	private ResourceLoader resourceLoader;
 	private Map<String, Schema> entitySchemaMap = new HashMap<>();
@@ -60,7 +64,14 @@ public class JsonValidationServiceImpl implements IValidate {
 	public boolean validate(APIMessage apiMessage) throws MiddlewareHaltException {
 	    Schema schema = getEntitySchema(apiMessage.getRequest().getEntityType());
 		JSONObject obj = new JSONObject(new JSONTokener(apiMessage.getRequest().getRequestMapAsString()));
-		schema.validate(obj);
+		try {
+			schema.validate(obj); // throws a ValidationException if this object is invalid
+		} catch (ValidationException e) {
+			logger.error(e.getMessage() + " : " + e.getErrorMessage());
+			e.getCausingExceptions().stream()
+					.map(ValidationException::getMessage)
+					.forEach(logger::error);
+		}
 		return true;
 
 	}
