41c66581a48f3b467d96503009bffab2e4a1277c
pritha-tarento <pritha.chattopadhyay@tarento.com>
2018-12-06 19:35:55 +0530

    Issue #OS-130 Neo4jGraphProvider: @PostConstruct, @PreDestroy added

--- java/registry/src/main/java/io/opensaber/registry/sink/Neo4jGraphProvider.java
+++ java/registry/src/main/java/io/opensaber/registry/sink/Neo4jGraphProvider.java
@@ -1,5 +1,8 @@
 package io.opensaber.registry.sink;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.neo4j.driver.v1.AuthToken;
 import org.neo4j.driver.v1.AuthTokens;
@@ -14,9 +17,8 @@ import com.steelbridgelabs.oss.neo4j.structure.providers.Neo4JNativeElementIdPro
 
 import io.opensaber.registry.model.DBConnectionInfo;
 
+public class Neo4jGraphProvider extends DatabaseProvider {
 
-public class Neo4jGraphProvider extends DatabaseProvider{
-	
 	private Logger logger = LoggerFactory.getLogger(Neo4jGraphProvider.class);
 
 	private Driver driver;
@@ -24,32 +26,46 @@ public class Neo4jGraphProvider extends DatabaseProvider{
 
 	public Neo4jGraphProvider(DBConnectionInfo connection) {
 
-		AuthToken authToken = AuthTokens.basic(connection.getUsername(), connection.getPassword());
-        if (authToken != null) {
-            driver = GraphDatabase.driver(connection.getUri(),
-                    authToken);
-        } else {
-            driver = GraphDatabase.driver(connection.getUri(),
-                    AuthTokens.none());
-        }
+		try {
+			AuthToken authToken = AuthTokens.basic(connection.getUsername(), connection.getPassword());
+			if (authToken != null) {
+				driver = GraphDatabase.driver(connection.getUri(), authToken);
+			} else {
+				driver = GraphDatabase.driver(connection.getUri(), AuthTokens.none());
+			}
 
-		Neo4JElementIdProvider<?> idProvider = new Neo4JNativeElementIdProvider();
-		Neo4JGraph neo4JGraph = new Neo4JGraph(driver, idProvider, idProvider);
-		//neo4JGraph.setProfilerEnabled(profilerEnabled);
-		graph = neo4JGraph;
-		logger.info("Initializing remote graph db for "+connection.getShardId());	
+			Neo4JElementIdProvider<?> idProvider = new Neo4JNativeElementIdProvider();
+			Neo4JGraph neo4JGraph = new Neo4JGraph(driver, idProvider, idProvider);
+			// neo4JGraph.setProfilerEnabled(profilerEnabled);
+			graph = neo4JGraph;
+			logger.info("Initializing remote graph db for " + connection.getShardId());
+		} catch (Exception ex) {
+			logger.error("Exception when initializing Neo4J DB connection...", ex);
+			throw ex;
+		}
 	}
 
+	@Override
 	public Graph getGraphStore() {
 		return graph;
 	}
 
+	@PostConstruct
+	public void init() {
+		logger.info("**************************************************************************");
+		logger.info("Initializing Neo4J GraphDB instance ...");
+		logger.info("**************************************************************************");
+	}
+
+	@PreDestroy
 	public void shutdown() throws Exception {
+		logger.info("**************************************************************************");
+		logger.info("Gracefully shutting down Neo4J GraphDB instance ...");
+		logger.info("**************************************************************************");
 		graph.close();
 		if (driver != null) {
 			driver.close();
 		}
 	}
-
 
 }
