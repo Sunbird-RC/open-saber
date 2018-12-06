70c4b7b9886e241869537e6ceb9426de466100af
pritha-tarento <pritha.chattopadhyay@tarento.com>
2018-12-06 17:24:34 +0530

    Issue #OS-130 javadoc @DBConnectioInfoMgr

--- java/registry/src/main/java/io/opensaber/registry/model/DBConnectionInfoMgr.java
+++ java/registry/src/main/java/io/opensaber/registry/model/DBConnectionInfoMgr.java
@@ -5,12 +5,23 @@ import java.util.List;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
+/**
+ * 
+ * @author Pritha Chattopadhyay
+ * Auto populate/bind the field values from yaml properties.
+ *
+ */
 @Component
 @ConfigurationProperties(prefix = "database")
 public class DBConnectionInfoMgr {
 	
+	/**
+	 * get the provider property value from properties or yaml file
+	 */
 	private String provider;
+	/**
+	 * get the connectionInfo list of properties values from yaml/properties file.
+	 */
 	private List<DBConnectionInfo> connectionInfo = new ArrayList<>();
 	
 	public DBConnectionInfoMgr(){
@@ -34,6 +45,11 @@ public class DBConnectionInfoMgr {
 	}
 
 	//TODO: add this as a map, for optimized search.
+	/**
+	 * To provide a connection info on based of a shard identifier(name)
+	 * @param name
+	 * @return
+	 */
 	public DBConnectionInfo getDatabaseConnection(String name){
 		for(DBConnectionInfo con: connectionInfo){
 			if(con.getName().equalsIgnoreCase(name))
