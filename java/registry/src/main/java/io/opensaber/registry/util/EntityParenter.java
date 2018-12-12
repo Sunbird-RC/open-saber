c43dc4013e1baceada313523884a3ba05011c3ee
indrajra-ilimi <rajeshr@ilimi.in>
2018-12-12 11:26:03 +0530

    Issue #OS-141 Add base classes to support parent grouping

---
+++ java/registry/src/main/java/io/opensaber/registry/util/EntityParenter.java
@@ -0,0 +1,74 @@
+package io.opensaber.registry.util;
+
+import io.opensaber.registry.model.DBConnectionInfo;
+import io.opensaber.registry.model.DBConnectionInfoMgr;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Component("entityParenter")
+public class EntityParenter {
+    @Autowired
+    private DefinitionsManager definitionsManager;
+
+    @Autowired
+    private DBConnectionInfoMgr dbConnectionInfoMgr;
+
+    private Set<String> defintionNames = definitionsManager.getAllKnownDefinitions();
+    private List<DBConnectionInfo> dbConnectionInfoList = dbConnectionInfoMgr.getConnectionInfo();
+
+    /**
+     * Holds information about a definition and a list of ShardParents
+     */
+    private HashMap<String, ShardParentInfoList> definitionShardParentMap;
+
+    /**
+     * Creates the parent vertex in all the shards for all default definitions
+     * @return
+     */
+    public Optional<String> createKnownParenters(Graph graph) {
+        Optional<String> result;
+        String msg = null;
+
+        dbConnectionInfoList.forEach(dbConnectionInfo -> {
+            // Get the shard
+
+            defintionNames.forEach(defintionName -> {
+                String parentLabel = ParentLabelGenerator.getLabel(defintionName);
+                TPGraphMain.createParentVertex(graph, parentLabel);
+            });
+        });
+
+        result = Optional.of(msg);
+        return result;
+    }
+
+    /**
+     * Reads the database and learns the mappings. This is expected to be called
+     * just once.
+     * @return
+     */
+    public void identifyDefinitionParents() {
+        defintionNames.forEach(defintionName -> {
+        // Create the vertex with label if not already found
+            ShardParentInfoList parentInfoList = new ShardParentInfoList();
+
+            dbConnectionInfoList.forEach(dbConnectionInfo -> {
+                // TODO
+                // Get the shard and fetch uuid from label
+
+                String shardId = "";
+                String uuid = "";
+                parentInfoList.getParentInfos().add(new ShardParentInfo(shardId, uuid));
+            });
+
+            definitionShardParentMap.put(defintionName, parentInfoList);
+        });
+    }
+
+}
