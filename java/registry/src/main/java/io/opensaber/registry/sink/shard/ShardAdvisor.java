5a73f936176799f30250f516de983ec8aa73adbd
pritha-tarento <pritha.chattopadhyay@tarento.com>
2018-12-24 16:42:22 +0530

    issue #OS-156 Addressed PR review comments

--- java/registry/src/main/java/io/opensaber/registry/sink/shard/ShardAdvisor.java
+++ java/registry/src/main/java/io/opensaber/registry/sink/shard/ShardAdvisor.java
@@ -2,22 +2,39 @@ package io.opensaber.registry.sink.shard;
 
 import io.opensaber.registry.model.DBConnectionInfoMgr;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ShardAdvisor {
-	
+	private static Logger logger = LoggerFactory.getLogger(ShardAdvisor.class);
 	private DBConnectionInfoMgr dbConnectionInfoMgr;
 	private Map<String, IShardAdvisor> advisors = new HashMap<String, IShardAdvisor>();
-	private final static String DEFAULT_SHARD_ADVISOR = "DefaultShardAdvisor";
-	private final static String SERIAL_NUMBER_SHARD_ADVISOR = "SerialNumberShardAdvisor";
+
 
 	public ShardAdvisor(DBConnectionInfoMgr dbConnectionInfoMgr) {
 		this.dbConnectionInfoMgr = dbConnectionInfoMgr;
+		try {
+			registerShardAdvisor();
+		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException
+				| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+			logger.error("While registering shard advisor with name {}, exception occured: {}",
+					dbConnectionInfoMgr.getShardAdvisor(), e);
+		}
+	}
+
+	private void registerShardAdvisor() throws ClassNotFoundException, NoSuchMethodException, SecurityException,
+			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		String advisorClassName = dbConnectionInfoMgr.getShardAdvisor();
+		Class<?> clazz = Class.forName("io.opensaber.registry.sink.shard." + advisorClassName);
+		IShardAdvisor advisor = (IShardAdvisor) clazz.getConstructor(DBConnectionInfoMgr.class)
+				.newInstance(dbConnectionInfoMgr);
+		logger.info("Registered shard advisor class: "+advisorClassName);
+		advisors.put(advisorClassName, advisor);
 		
-		// Registers all the shardAdvisors by class name.		
-		advisors.put(DEFAULT_SHARD_ADVISOR, new DefaultShardAdvisor(dbConnectionInfoMgr));
-		advisors.put(SERIAL_NUMBER_SHARD_ADVISOR, new SerialNumberShardAdvisor(dbConnectionInfoMgr));
 	}
 
 	/**
