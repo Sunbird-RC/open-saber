8b97f6df31af37a11a77bb0111ae5ea6e6a395c9
pritha-tarento <pritha.chattopadhyay@tarento.com>
2018-12-25 12:09:09 +0530

    Issue #OS-155 resolved codacy issue

--- java/registry/src/main/java/io/opensaber/registry/util/ShardLabelHelper.java
+++ java/registry/src/main/java/io/opensaber/registry/util/ShardLabelHelper.java
@@ -3,36 +3,38 @@ package io.opensaber.registry.util;
 public class ShardLabelHelper {
 	private final static String SEPARATOR = "-";
 	private final static String REGEX_UUID = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
+
 	/**
 	 * Forms label with shard info
+	 * 
 	 * @param shardLabel
 	 * @param recordId
 	 * @return
 	 */
 	public static String getLabel(String shardLabel, String recordId) {
-        return shardLabel + SEPARATOR + recordId;
-    }
+		return shardLabel + SEPARATOR + recordId;
+	}
+
 	/**
 	 * Extract shard info
+	 * 
 	 * @param label
 	 * @return
 	 */
-    public static String getShardName(String label) {
-        return label.substring(0, label.indexOf(SEPARATOR));
-    }
-    /**
-     * verifies label of this shard type
-     * @param label
-     * @param recordId
-     * @return
-     */
-    public static boolean isShardLabel(String label) {
-    	String uuid = label.substring(label.indexOf(SEPARATOR)+1, label.length());
-    	if (uuid.matches(REGEX_UUID)) {
-    		return true;
-    	} else{
-        	return false;
-    	}
-    }
+	public static String getShardName(String label) {
+		return label.substring(0, label.indexOf(SEPARATOR));
+	}
+
+	/**
+	 * verifies label of this shard type
+	 * 
+	 * @param label
+	 * @param recordId
+	 * @return
+	 */
+	public static boolean isShardLabel(String label) {
+		String uuid = label.substring(label.indexOf(SEPARATOR) + 1, label.length());
+		return uuid.matches(REGEX_UUID);
+	}
 
 }
