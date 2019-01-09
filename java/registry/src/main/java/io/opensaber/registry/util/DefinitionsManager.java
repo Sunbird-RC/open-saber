5c1cbf1416d0d6845f13b27f4f568771c56e0d7a
pritha-tarento <pritha.chattopadhyay@tarento.com>
2019-01-09 17:17:05 +0530

    Issue #OS-157 Introduced OSConfigProperties for holding _osConfig properties

--- java/registry/src/main/java/io/opensaber/registry/util/DefinitionsManager.java
+++ java/registry/src/main/java/io/opensaber/registry/util/DefinitionsManager.java
@@ -1,9 +1,8 @@
 package io.opensaber.registry.util;
 
-import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,74 +24,75 @@ public class DefinitionsManager {
 
     @Autowired
     private DefinitionsReader definitionsReader;
-    private Map<String, SchemaDefination> schemaDefinationMap = new HashMap<>();
+    private Map<String, SchemaDefinition> schemaDefinationMap = new HashMap<>();
 
     /**
      * Loads the definitions from the _schemas folder
      */
     @PostConstruct
-    public void loadSchemaDefination(){
+    public void loadSchemaDefination() {
         try {
             Resource[] resources = definitionsReader.getResources("classpath:public/_schemas/*.json");
             for (Resource resource : resources) {
                 String jsonContent = getContent(resource);
                 JSONObject jsonObject = new JSONObject(jsonContent);
-                SchemaDefination schemaDefination = new SchemaDefination(jsonObject);
+                SchemaDefinition schemaDefination = new SchemaDefinition(jsonObject);
                 schemaDefinationMap.putIfAbsent(schemaDefination.getTitle(), schemaDefination);
             }
 
-        } catch (JSONException |IOException ioe) {
+        } catch (JSONException | IOException ioe) {
             logger.error("Cannot load json resources. Validation can't work");
         }
     }
+
     /**
      * Returns the title for all schema loaded
+     * 
      * @return
      */
-    public Set<String> getAllKnownDefinitions() {       
+    public Set<String> getAllKnownDefinitions() {
         return schemaDefinationMap.keySet();
     }
+
     /**
      * Returns all schema definitions that are loaded
+     * 
      * @return
      */
-    public List<SchemaDefination> getAllSchemaDefinations() {
-        List<SchemaDefination> schemaDefinations = new ArrayList<>();
-        for (Entry<String, SchemaDefination> entry : schemaDefinationMap.entrySet()) {
+    public List<SchemaDefinition> getAllSchemaDefinations() {
+        List<SchemaDefinition> schemaDefinations = new ArrayList<>();
+        for (Entry<String, SchemaDefinition> entry : schemaDefinationMap.entrySet()) {
             schemaDefinations.add(entry.getValue());
         }
         return schemaDefinations;
     }
+
     /**
-     * Provide a schemaDefinition by given title which is already loaded 
+     * Provide a schemaDefinition by given title which is already loaded
+     * 
      * @param title
      * @return
      */
-    public SchemaDefination getSchemaDefination(String title) {
+    public SchemaDefinition getSchemaDefination(String title) {
         return schemaDefinationMap.getOrDefault(title, null);
     }
 
     /**
-     * Returns a content of resource 
+     * Returns a content of resource
+     * 
      * @param resource
      * @return
      */
     private String getContent(Resource resource) {
-        InputStream in;
+        String content = null;
         try {
-            in = resource.getInputStream();
-            ByteArrayOutputStream result = new ByteArrayOutputStream();
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = in.read(buffer)) != -1) {
-                result.write(buffer, 0, length);
-            }
-            return result.toString(StandardCharsets.UTF_8.name());
-
+            File file = resource.getFile();
+            content = new String(Files.readAllBytes(file.toPath()));
+            logger.info(resource.getFilename() + "Content: " + content);
         } catch (IOException e) {
             logger.error("Cannot load resource " + resource.getFilename());
 
         }
-        return null;
+        return content;
     }
 }
---
+++ java/registry/src/main/java/io/opensaber/registry/util/OSConfigProperties.java
@@ -0,0 +1,44 @@
+package io.opensaber.registry.util;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Holds _osconfig properties for a schema  
+ *
+ */
+public class OSConfigProperties {
+    
+    private String comment;
+    private List<String> privateFields =  new ArrayList<>();
+    private List<String> signedFields =  new ArrayList<>();
+
+/*    public OSConfigProperties(){
+        privateFields =
+        signedFields = new ArrayList<>();
+    }
+*/
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public List<String> getPrivateFields() {
+        return privateFields;
+    }
+
+    public void setPrivateFields(List<String> privateFields) {
+        this.privateFields = privateFields;
+    }
+
+    public List<String> getSignedFields() {
+        return signedFields;
+    }
+
+    public void setSignedFields(List<String> signedFields) {
+        this.signedFields = signedFields;
+    }
+    
+}
