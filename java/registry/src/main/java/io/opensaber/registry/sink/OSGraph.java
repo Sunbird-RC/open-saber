86986afe4c54afa02662bdd6220996230cc35e90
indrajra-ilimi <rajeshr@ilimi.in>
2019-02-13 17:26:35 +0530

    Issue #OS-178 Support transactions for TinkerGraph

--- java/registry/src/main/java/io/opensaber/registry/sink/OSGraph.java
+++ java/registry/src/main/java/io/opensaber/registry/sink/OSGraph.java
@@ -5,8 +5,11 @@ import org.apache.tinkerpop.gremlin.structure.Graph;
 public class OSGraph implements AutoCloseable {
     private Graph graph;
     private boolean closeRequired;
+    private boolean supportsTransaction = false;
 
-    protected OSGraph() {}
+    protected OSGraph() {
+        supportsTransaction = graph.features().graph().supportsTransactions();
+    }
 
     public OSGraph (Graph g, boolean close) {
         graph = g;
@@ -14,7 +17,7 @@ public class OSGraph implements AutoCloseable {
     }
 
     public void close() throws Exception {
-        if (graph != null && graph.tx().isOpen()) {
+        if (graph != null && supportsTransaction && graph.tx().isOpen()) {
             graph.tx().close();
         }
 
