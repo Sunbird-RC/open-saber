2376458c3a413fe4152c77d6dc0b752b1dfc9a5f
aishwa8141 <a.aish1997@gmail.com>
2019-06-12 15:47:29 +0530

    lang changes

--- ui-sample/utils.js
+++ ui-sample/utils.js
@@ -1,17 +1,24 @@
 function dataLocalizeRefresh(lang, fullLangText) {
+  console.log('data', lang, fullLangText)
+  document.getElementById("langSelected").innerHTML = fullLangText
+
   var opts = { language: lang, pathPrefix: "./lang" };
   $("[data-localize]").localize("stringData", opts);
-  document.getElementById("langSelected").innerText = fullLangText
+
   console.log("dataLocalizeRefreshEnd " + lang);
 }
 
 function dataLocalizeRefreshDefault() {
-  var langSelected = document.getElementById("langSelected")
+  var langSelected = document.getElementById("langSelected").innerText
+  console.log(langSelected)
   var lang = "en"
   if (langSelected != null) {
     lang = langSelected.innerText
+    console.log('lo', langSelected, lang)
+
   }
   var opts = { language: lang, pathPrefix: "./lang" };
   $("[data-localize]").localize("stringData", opts);
   console.log("dataLocalizeRefreshEnd " + lang);
+
 }
