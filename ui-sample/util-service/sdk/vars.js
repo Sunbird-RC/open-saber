e3c81298e0c02882758a72f1d3d7ffd36049a381
aishwa8141 <a.aish1997@gmail.com>
2020-01-27 14:03:31 +0530

    removed env vars value

--- ui-sample/util-service/sdk/vars.js
+++ ui-sample/util-service/sdk/vars.js
@@ -2,13 +2,13 @@ const config = {
     "dev": {
         "keycloak": {
             "url": process.env.keycloak_url || "http://localhost:8443", 
-            "realmName": process.env.keycloak_realmName || "NIITRegistry",
+            "realmName": process.env.keycloak_realmName,
             "clientId": "utils",
             "clientSecret": process.env.keycloak_clientSecret || "f6ce7466-b04f-4ccf-b986-e9c61e5fb26b"
         },
         "keycloak_epr": {
             "url": process.env.keycloak_epr_url || "http://localhost:8443",
-            "realmName": process.env.keycloak_epr_realmName || "PartnerRegistry",
+            "realmName": process.env.keycloak_epr_realmName,
             "clientId": "utils",
             "clientSecret": process.env.keycloak_epr_clientSecret || "9ebc2fc1-ced9-4774-a661-7e2c59991cfe"
         },
@@ -40,7 +40,7 @@ const logger = require('./log4j')
 module.exports.getAllVars = function (envName) {
     var environment = envName
     if (envName === undefined) {
-        environment = 'development'
+        environment = 'dev'
     }
     logger.info("Service running in mode = " + environment)
     return config[environment]
