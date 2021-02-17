04ea36229d8291819da9af9d064c8dfc63cc04f3
satish Nirmalkar <satishkumar.nirmalkar@tarento.com>
2021-02-17 15:15:04 +0530

    OS-master-fix

--- configure-dependencies.sh
+++ configure-dependencies.sh
@@ -2,5 +2,14 @@
 echo "Configuring dependencies before build"
 cp java/registry/src/main/resources/application.yml.sample java/registry/src/main/resources/application.yml
 cp java/registry/src/main/resources/frame.json.sample java/registry/src/main/resources/frame.json
-#cp java/registry/src/main/resources/audit_frame.json.sample java/registry/src/main/resources/audit_frame.json
+
+default_schema=true
+schema_dir='java/registry/src/main/resources/public/_schemas'
+
+for file in "$schema_dir"/*; do
+   new_file=$(basename "$file")
+   new_file=${new_file%.sample}
+   cp $file "$schema_dir""/""$new_file"
+done
+
 echo "Configuration of dependencies completed"
