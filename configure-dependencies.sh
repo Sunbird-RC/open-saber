765216930bab890f925af78c7f45d1fd5fed4ec8
Trijeet Modak <uniquetrij@gmail.com>
2021-09-07 19:36:21 +0530

    no-args check, default schema generation on args
    Added check if no arguments are provided, defaults to false. Prevent default schema generation if argument is false. Automatically creates schema directory to prevent "no such file or directory" error if argument is true.

--- configure-dependencies.sh
+++ configure-dependencies.sh
@@ -3,18 +3,19 @@ echo "Configuring dependencies before build"
 cp java/registry/src/main/resources/application.yml.sample java/registry/src/main/resources/application.yml
 cp java/registry/src/main/resources/frame.json.sample java/registry/src/main/resources/frame.json
 
-if "$1" == "true":
-then
+if [ $# -gt 0 ] && "$1" == "true"; then
  default_schema=true
 else
  default_schema=false
 fi
 
+echo "Using default schema: $default_schema"
+
 schema_dir='java/registry/src/main/resources/public/_schemas'
 schema_sample_dir='java/registry/src/main/resources/public/_schemas_sample'
 
-if $defaul_schema
-then
+if [ "$default_schema" = true ]; then
+  mkdir -p "$schema_dir"
   for file in "$schema_sample_dir"/*; do
     old_file="$(basename "$file")"
     
